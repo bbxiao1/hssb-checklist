af68b4c8818f6676273d10d5f73aedfd22c34d9e
Xiao Ba <bbxiao1@gmail.com>
2020-11-16 20:01:17 -0700

    idk whatever

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+interface Ship {
+  id: number,
+  name: string
+}
+
 function App() {
+  let ships: Ship[] = [
+    {id: 1, name: "Mackerel"},
+    {id: 2, name: "Gecko"}
+  ]
+  var selectedShip: Ship;
+
+  const didSelectShip = (ship: Ship) => {
+    selectedShip = ship
+  }
+
+  let renderedShips = ships.map((ship) => {
+    return <li
+             key={ship.id}
+             className="list-group-item ship-list-name">
+               <div className="clicky" onClick={e => didSelectShip(ship) }>
+                 {ship.name}
+               </div>
+           </li>
+  })
+
+  let renderedChecklist = () => {
+    if (selectedShip) {
+      return <div className="no-ship">{selectedShip.name}</div>
+    } else {
+      return <div className="no-ship">Please select a ship</div>
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Hardspace: Shipbreaker
+        </h2>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <ul className="list-group list-group-flush">
+              {renderedShips}
+            </ul>
+          </div>
+          <div className="col">
+            {renderedChecklist()}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
